f3ed77f9bd54bdbc76ef9698ee31c0e86fe93fee
Tomas Macia <tomas.macia@mercadolibre.com>
2024-10-18 01:39:12 -0300

    handler

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.routes import payments, wallets
 
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 
 app.include_router(payments.router, prefix="/payments", tags=["payments"])
 app.include_router(wallets.router, prefix="/wallets", tags=["wallets"])
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+handler = Mangum(app)
